8c65ac160e26f94595c6c961ae6cd78425fa1941
hans-zanecoder <hans@zanecoder.com>
2025-05-23 04:37:02 +0800

    Modified Docker and Config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,15 +4,22 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5002/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` : 'http://127.0.0.1:5002/api/:path*',
       },
       {
         source: '/uploads/:path*',
-        destination: 'http://127.0.0.1:5002/uploads/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/uploads/:path*` : 'http://127.0.0.1:5002/uploads/:path*',
       }
     ];
   },
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true // Only use during development
+  },
+  eslint: {
+    ignoreDuringBuilds: true // Only use during development
+  },
+  output: 'standalone',
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
